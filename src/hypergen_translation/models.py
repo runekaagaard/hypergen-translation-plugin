da11b54b50252bd831a9806625890f9d5e59fa65
Rune Kaagaard <rumi.kg@gmail.com>
2023-12-05 14:17:26 +0100

    chatgpt models.

---
+++ src/hypergen_translation/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+
+from django.db import models
+from django.utils import timezone
+
+class String(models.Model):
+    added = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    value = models.TextField()
+
+    def __str__(self):
+        return self.value
+
+    class Meta:
+        verbose_name = "String"
+        verbose_name_plural = "Strings"
+
+class Occurrence(models.Model):
+    added = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    string = models.ForeignKey(String, on_delete=models.PROTECT)
+    file_path = models.CharField(max_length=1024)
+    python_path = models.CharField(max_length=1024)
+    line_number = models.IntegerField()
+
+    class Meta:
+        verbose_name = "Occurrence"
+        verbose_name_plural = "Occurrences"
+        unique_together = ('string', 'file_path', 'line_number')
+
+    def __str__(self):
+        return f"{self.file_path}:{self.line_number}"
+
+class Language(models.Model):
+    added = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    language_code = models.CharField(max_length=2)
+
+    class Meta:
+        verbose_name = "Language"
+        verbose_name_plural = "Languages"
+
+    def __str__(self):
+        return self.language_code
+
+class TranslationStatus(models.Model):
+    status = models.CharField(max_length=10)
+
+    class Meta:
+        verbose_name = "Translation Status"
+        verbose_name_plural = "Translation Statuses"
+
+    def __str__(self):
+        return self.status
+
+class Translation(models.Model):
+    added = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    added_by = models.ForeignKey('auth.User', related_name='translations_added', on_delete=models.PROTECT,
+                                 null=True, blank=True)
+    updated_by = models.ForeignKey('auth.User', related_name='translations_updated', on_delete=models.PROTECT,
+                                   null=True, blank=True)
+    string = models.ForeignKey(String, on_delete=models.PROTECT)
+    language = models.ForeignKey(Language, on_delete=models.PROTECT)
+    status = models.ForeignKey(TranslationStatus, on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name = "Translation"
+        verbose_name_plural = "Translations"
+
+    def __str__(self):
+        return f"{self.string} - {self.language}"
